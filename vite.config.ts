6ecb760791d3958900cd611727abea82f1d161da
Luis Crespo <lcrespom@gmail.com>
2026-01-31 08:28:43 +0100

    Removed eslint issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
 /// <reference types="vitest/config" />
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
